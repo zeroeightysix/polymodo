1fcb0f475b87f9b7393e18c17a419cf0099e36ae
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2025-05-21 02:23:14 +0200

    rename (confusing) `update` to `run_ui`

--- src/windowing/surface.rs
+++ src/windowing/surface.rs
@@ -99,7 +99,7 @@ impl Surface {
             })
             .forget_lifetime();
 
-        let output = self.update(ctx, render_ui);
+        let output = self.run_ui(ctx, render_ui);
 
         let prims = ctx.tessellate(output.shapes, output.pixels_per_point);
         {
@@ -143,7 +143,7 @@ impl Surface {
         Ok(output.platform_output)
     }
 
-    fn update(
+    fn run_ui(
         &mut self,
         ctx: &Context,
         render_ui: impl FnMut(&Context) + Sized,
